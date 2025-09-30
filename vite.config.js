22827c09ebdc83effbd41037b2d572aa9cd7a7c2
wilson <wvasquezg1@miumg.edu.gt>
2025-09-30 08:36:44 -0600

    complemento
    visualizacion en aws s3 static website hosting

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', //cambio, para que lo pueda leer aws s3 static website hosting
   plugins: [react()],
 })
